dd656781cd766438d40f54002a0027859dcfadc3
Aleksandra Jercha <aleks.jercha@gmail.com>
2021-04-06 21:04:15 +0200

    metoda fetch

--- src/Components/SectionPreview/SectionPreview.js
+++ src/Components/SectionPreview/SectionPreview.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import './SectionPreview.css';
 import luna from './../Assets/luna.png'; // with import
+import { render } from 'react-dom';
 
 
 
 
-function SectionPreview(props) {
-return <div class="section">
- <h2>{props.name}</h2>
- <img class="pic" src={luna}></img>
- <img class="pic" src={luna}></img>  
- <img class="pic" src={luna}></img>  
-</div>
+class SectionPreview extends React.Component  {
+    componentDidMount() {
+        fetch(`https://api.unsplash.com/search/photos?query=${this.props.name}&client_id=`)
+        .then(response => {
+            
+            return response.json();
+        });
+    }
+
+    render() {
+        return <div className="section">
+            <h2>{this.props.name}</h2>
+            <img class="pic" src={luna}></img>
+            <img class="pic" src={luna}></img>
+            <img class="pic" src={luna}></img>
+        </div>
+    }
 }
+
 export default SectionPreview;
-
-
