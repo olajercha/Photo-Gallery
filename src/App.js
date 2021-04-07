6b62f6fb346cc608353361d4c7567166d1bec830
Aleksandra Jercha <aleks.jercha@gmail.com>
2021-04-07 17:28:05 +0200

    dodanie nowych sekcji

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ return <div>
  <header>PHOTO GALLERY</header>   
  <SectionPreview name="Puppies"></SectionPreview>
  <SectionPreview name="Kittens"></SectionPreview>
+ <SectionPreview name="Penguins"></SectionPreview>
+ <SectionPreview name="Ponies"></SectionPreview>
+ <SectionPreview name="Owls"></SectionPreview>
 </div>
 }export default App;
 
