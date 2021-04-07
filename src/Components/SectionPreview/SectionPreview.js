c8cbc04d942a8a3dfa72ea0e383400c15f1d79fa
Aleksandra Jercha <aleks.jercha@gmail.com>
2021-04-07 17:23:09 +0200

    dodanie pobierania zdjęć

--- src/Components/SectionPreview/SectionPreview.js
+++ src/Components/SectionPreview/SectionPreview.js
@@ -7,20 +7,28 @@ import { render } from 'react-dom';
 
 
 class SectionPreview extends React.Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: []}
+    };
+
+      
     componentDidMount() {
         fetch(`https://api.unsplash.com/search/photos?query=${this.props.name}&client_id=`)
-        .then(response => {
-            
-            return response.json();
+        .then(response => response.json())
+        .then((json)=> {
+            const imgUrls = json.results.map((result)=> result.urls.full);
+            this.setState({images: imgUrls});
+            console.log(json);
         });
     }
 
     render() {
+        const imageItems = this.state.images.map((src) => <img class="pic" src={src}></img>    );
         return <div className="section">
             <h2>{this.props.name}</h2>
-            <img class="pic" src={luna}></img>
-            <img class="pic" src={luna}></img>
-            <img class="pic" src={luna}></img>
+            {imageItems}
         </div>
     }
 }
